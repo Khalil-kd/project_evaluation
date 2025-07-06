78361443ea3ebf87a8a4469e81991e62fcdba7c5
Khalil-kd <kadriahmedkhalil@gmail.com>
2025-07-06 13:40:58 +0200

    Ajout du fichier principal app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard Ventes", layout="wide")
+st.title("📊 Application d'évaluation des ventes - McDonald's vs Burger King")
+st.write("Bienvenue dans votre tableau de bord interactif.")
